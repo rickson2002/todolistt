e41bb88e1f93fa43dc4b737b20d0f4a53fad44d7
Rickson De Souza Ferreira <rs111349@gmail.com>
2023-10-28 12:11:40 -0300

    Dockerfile deploy

---
+++ src/test/java/br/com/ricksonsouza/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ricksonsouza.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
